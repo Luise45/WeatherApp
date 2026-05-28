a7f0d1c82afc75e972d5753e6bdaa6097bd74c41
Luise <luisetabatt@gmail.com>
2026-05-28 08:40:37 +0200

    changes in app routing to change to dashboard view

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,17 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
-import WeatherPage from "./Weather/Weather";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/weather" element={<WeatherPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
-
